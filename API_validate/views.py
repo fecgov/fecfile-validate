35ac589eec5b961e9f61ccb5de9415926d649500
jpraveenkmrSalient <41488234+jpraveenkmrSalient@users.noreply.github.com>
2018-11-13 12:59:40 -0500

    Update views.py

--- API_validate/views.py
+++ API_validate/views.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 
 @api_view(['POST'])
-def validate_f99(request):
+def validate_f99(data.json):
     # # get all comm info
     # if request.method == 'GET':
     #     comm_info = CommitteeInfo.objects.all()
@@ -32,22 +32,22 @@ def validate_f99(request):
         with open('data.json') as f:
             data = json.load(f)
             data = {
-                'committeeid': request.data.get('committeeid'),
-                'committeename': request.data.get('committeename'),
-                'street1': request.data.get('street1'),
-                'street2': request.data.get('street2'),
-                'city': request.data.get('city'),
-                'state': request.data.get('state'),
-                'text': request.data.get('text'),
-                'reason' :request.data.get('reason'),
-                'zipcode': request.data.get('zipcode'),
-                'treasurerlastname': request.data.get('treasurerlastname'),
-                'treasurerfirstname': request.data.get('treasurerfirstname'),
-                'treasurermiddlename': request.data.get('treasurermiddlename'),
-                'treasurerprefix': request.data.get('treasurerprefix'),
-                'treasurersuffix': request.data.get('treasurersuffix'),
-                'email_on_file' : request.data.get('email_on_file'),
-                'file': request.data.get('file'),
+                'committeeid': data.get('committeeid'),
+                'committeename': data.get('committeename'),
+                'street1': data.get('street1'),
+                'street2': data.get('street2'),
+                'city': data.get('city'),
+                'state': data.get('state'),
+                'text': data.get('text'),
+                'reason': data.get('reason'),
+                'zipcode': data.get('zipcode'),
+                'treasurerlastname': data.get('treasurerlastname'),
+                'treasurerfirstname': data.get('treasurerfirstname'),
+                'treasurermiddlename': data.get('treasurermiddlename'),
+                'treasurerprefix': data.get('treasurerprefix'),
+                'treasurersuffix': data.get('treasurersuffix'),
+                'email_on_file': data.get('email_on_file'),
+                'file': data.get('file'),
             }
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -60,62 +60,67 @@ def validate_f99(request):
 
     errormess = []
 
-    if comm.committeename!=request.data.get('committeename'):
+    if comm.committeename!=data.get('committeename'):
         errormess.append('Committee Name does not match the Form 1 data.')
         
-    if comm.street1!=request.data.get('street1'):
+    if comm.street1!=data.get('street1'):
         errormess.append('Street1 does not match the Form 1 data.')
             
-    if 'street2' in request.data and comm.street2!=request.data.get('street2'):
+    if 'street2' in data and comm.street2!=data.get('street2'):
         errormess.append('Street2 does not match the Form 1 data.')
         
-    if comm.city!=request.data.get('city'):
+    if comm.city != data.get('city'):
         errormess.append('City does not match the Form 1 data.')
-            
-    if comm.state!=request.data.get('state'):
+
+    if comm.state != data.get('state'):
         errormess.append('State does not match the Form 1 data.')
-    
-    if comm.zipcode!=int(request.data.get('zipcode')):
+
+    if comm.zipcode != int(data.get('zipcode')):
         errormess.append('Zipcode does not match the Form 1 data.')
 
-    if comm.treasurerlastname!=request.data.get('treasurerlastname'):
+    if comm.treasurerlastname != data.get('treasurerlastname'):
         errormess.append('Treasurer Last Name does not match the Form 1 data.')
-            
-    if comm.treasurerfirstname!=request.data.get('treasurerfirstname'):
-        errormess.append('Treasurer First Name does not match the Form 1 data.')
-        
-    if 'treasurermiddlename' in request.data and comm.treasurermiddlename!=request.data.get('treasurermiddlename'):
-        errormess.append('Treasurer Middle Name does not match the Form 1 data.')
-        
-    if 'treasurerprefix' in request.data and comm.treasurerprefix!=request.data.get('treasurerprefix'):
+
+    if comm.treasurerfirstname != data.get('treasurerfirstname'):
+        errormess.append(
+            'Treasurer First Name does not match the Form 1 data.')
+
+    if 'treasurermiddlename' in data and comm.treasurermiddlename != data.get('treasurermiddlename'):
+        errormess.append(
+            'Treasurer Middle Name does not match the Form 1 data.')
+
+    if 'treasurerprefix' in data and comm.treasurerprefix != data.get('treasurerprefix'):
         errormess.append('Treasurer Prefix does not match the Form 1 data.')
-        
-    if 'treasurersuffix' in request.data and comm.treasurersuffix!=request.data.get('treasurersuffix'):
+
+    if 'treasurersuffix' in data and comm.treasurersuffix != data.get('treasurersuffix'):
         errormess.append('Treasurer Suffix does not match the Form 1 data.')
-        
-    if len(request.data.get('text'))>20000:
+
+    if len(data.get('text')) > 20000:
         errormess.append('Text greater than 20000.')
 
-    if len(request.data.get('text'))==0:
+    if len(data.get('text')) == 0:
         errormess.append('Text field is empty.')
-        
-    conditions = [request.data.get('reason')=='MST', request.data.get('reason')=='MSM', request.data.get('reason')=='MSI', request.data.get('reason')=='MSW']
+
+    conditions = [data.get('reason') == 'MST', data.get('reason') == 'MSM', data.get('reason') == 'MSI', data.get('reason') == 'MSW']
     if not any(conditions):
         errormess.append('Reason does not match the pre-defined codes.')
-    
-    #pdf validation for type, extension and size
-    if 'file' in request.data:
+
+    # pdf validation for type, extension and size
+    if 'file' in data:
         valid_mime_types = ['application/pdf']
-        file = request.data.get('file')
+        file = data.get('file')
         file_mime_type = magic.from_buffer(file.read(1024), mime=True)
         if file_mime_type not in valid_mime_types:
-            errormess.append('This is not a pdf file type. Kindly open your document using a pdf reader before uploading it.')
+            errormess.append(
+                'This is not a pdf file type. Kindly open your document using a pdf reader before uploading it.')
         valid_file_extensions = ['.pdf']
         ext = os.path.splitext(file.name)[1]
         if ext.lower() not in valid_file_extensions:
-            errormess.append('Unacceptable file extension. Only files with .pdf extensions are accepted.')
+            errormess.append(
+                'Unacceptable file extension. Only files with .pdf extensions are accepted.')
         if file._size > 33554432:
-            errormess.append('The File size is more than 32 MB. Kindly reduce the size of the file before you upload it.')    
+            errormess.append(
+                'The File size is more than 32 MB. Kindly reduce the size of the file before you upload it.')    
             
     if len(errormess)==0:
         errormess.append('Validation successful!')
