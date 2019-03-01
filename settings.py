f8f2de0efc505dbd1d64a6c89495de6c41dc482b
develop <praveen.jinka@ctr.salientcrgt.com>
2019-03-01 12:28:00 -0500

    Modified port number from 5000 to 8001

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 API_URL = os.environ.get('FEC_VALIDATE_URL', '127.0.0.1')
-API_PORT = int(os.environ.get('PORT', 5000))
+API_PORT = int(os.environ.get('PORT', 8001))
 
 LANGUAGE_CODE = 'en-us'
 
