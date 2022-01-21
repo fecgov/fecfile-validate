04b01cbfa85e8cca03d375e1f80bcc55d09420a0
toddlees <todd.lees@tcg.com>
2022-01-21 16:50:27 -0500

    adds version and spec object to json schema

--- bin/generate-starter-schema.py
+++ bin/generate-starter-schema.py
@@ -14,29 +14,42 @@ Note: Schema properties prefixed with "fec_" are not part of the JSON schema
 standard and are specific to the FEC data.
 """
 
+from enum import Enum
 import openpyxl
 import json
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description='Convert the FEC validation Excel spreadsheet into JSON schema documents.')
 parser.add_argument('excel_filename', help='an excel filename that will be parsed to generate JSON schema docs')
+parser.add_argument('--sheets-to-generate', help="a json file containing an array of sheet names to be parsed from the excel file")
+parser.add_argument('--version')
 args = parser.parse_args()
-FILENAME = args.excel_filename or "Form_3X_ Receipts_Vendor_10.20.2020.xlsx"
-print(FILENAME)
-SCHEMA_ID_PREFIX = "https://github.com/mjtravers/fecfile-online-sandbox/blob/main/validator/schema"
+EXCEL_FILENAME = args.excel_filename or "Form_3X_ Receipts_Vendor_10.20.2020.xlsx"
+SCHEMA_ID_PREFIX = "https://github.com/fecgov/fecfile-Validate/blob/main/schema"
+VERSION = args.version or "v0.0.0.0"
+SHEETS_TO_SKIP = ['All receipts', 'Version 8.3', 'SUMMARY OF CHANGES']
 
 # Column postions of fields in the spreadsheet row array
-COL_SEQ = 0
-FIELD_DESCRIPTION = 1
-TYPE = 2
-REQUIRED = 3
-SAMPLE_DATA = 4
-VALUE_REFERENCE = 5
-RULE_REFERENCE = 6
 
 
+class Columns(Enum):
+    COL_SEQ = 0
+    FIELD_DESCRIPTION = 1
+    TYPE = 2
+    REQUIRED = 3
+    AUTO_POPULATE = 4
+    SAMPLE_DATA = 5
+    VALUE_REFERENCE = 6
+    RULE_REFERENCE = 7
+    FIELD_FORM_ASSOCIATION = 8
 
-def convert_row_to_property(row):# noqa
+    def get(self, row, has_autopopulate):
+        index = self.value if has_autopopulate or self.value <= 3 else self.value - 1
+        value = row[index] if index < len(row) else None
+        return value.strip() if isinstance(value, str) else value
+
+def convert_row_to_property(row, sheet_has_autopopulate):# noqa
     """Take a row from the spreadsheet and convert it into a schema object.
 
     Args:
@@ -47,10 +60,19 @@ def convert_row_to_property(row):# noqa
         prop (dict): The property object to add to the schema.
     """
     prop = {}
-    field_type = row[TYPE].strip()
-    token = row[FIELD_DESCRIPTION].strip().replace(" ", "_").replace(".", "").replace("(", "").replace(")", "")
+    spec = {}
+    for col in Columns:
+        if col.get(row, sheet_has_autopopulate):
+            spec[col.name] = col.get(row, sheet_has_autopopulate)
 
-    prop["title"] = row[FIELD_DESCRIPTION].strip()
+    title = spec.get(Columns.FIELD_DESCRIPTION.name)
+    field_type = spec.get(Columns.TYPE.name)
+    required = spec.get(Columns.REQUIRED.name)
+    sample_data = spec.get(Columns.SAMPLE_DATA.name)
+    rule_ref = spec.get(Columns.RULE_REFERENCE.name)
+
+    token = title.replace("\n", "_").replace(" ", "_").replace(".", "").replace("(", "").replace(")", "")
+    prop["title"] = title
     prop["description"] = ""
 
     if field_type.startswith("AMT-"):
@@ -64,7 +86,7 @@ def convert_row_to_property(row):# noqa
         prop["maximum"] = int('9' * int(field_type.split('-')[1]))
 
     if field_type.startswith("A/N-") or field_type.startswith("A-"):
-        if field_type == "A-1" and isinstance(row[RULE_REFERENCE], str) and row[RULE_REFERENCE].strip() == "Check-box":
+        if field_type == "A-1" and rule_ref == "Check-box":
             prop["type"] = "boolean"
         else:
             length = field_type.split('-')[1].strip()
@@ -73,67 +95,66 @@ def convert_row_to_property(row):# noqa
             prop["maxLength"] = int(length)
             prop["pattern"] = f'^[ A-z0-9]{{0,{length}}}$'
 
-    if row[SAMPLE_DATA]:
-        prop["examples"] = [row[SAMPLE_DATA]]
+    if sample_data:
+        prop["examples"] = [sample_data]
 
-    prop["fec_col_seq"] = row[COL_SEQ]
-    prop["fec_form_line"] = "0"
-    prop["fec_type"] = field_type
-
-    if isinstance(row[REQUIRED], str):
-        prop["fec_requiredErrorLevel"] = row[REQUIRED].strip()
-        if "error" in row[REQUIRED].strip():
+    if required and "error" in required:
             schema["required"].append(token)
-    if isinstance(row[VALUE_REFERENCE], str):
-        prop["fec_valueReference"] = row[VALUE_REFERENCE].strip()
-    if isinstance(row[RULE_REFERENCE], str):
-        prop["fec_ruleReference"] = row[RULE_REFERENCE].strip()
 
+    prop["spec"] = spec
     return (token, prop)
 
 
-wb = openpyxl.load_workbook(FILENAME)
+wb = openpyxl.load_workbook(EXCEL_FILENAME)
+sheets_to_generate = None
+if args.sheets_to_generate is not None:
+    with open(os.path.join(os.getcwd(), args.sheets_to_generate), 'r') as f:
+        sheets_to_generate = json.load(f)
 
+print(sheets_to_generate)
+trans_type_hits = {}
 for ws in wb.worksheets:
-    if ws.title in ['All receipts', 'All Schedule A Transactions', 'Version 8.3', 'SUMMARY OF CHANGES']:
+    if ((sheets_to_generate is not None and ws.title not in sheets_to_generate)
+            or ws.title in SHEETS_TO_SKIP):
         continue
 
-    # we don't care about these now, but will in the future
-    if ws.cell(3, 5).value is not None and ws.cell(3, 5).value.strip() == 'Auto populate':
-        continue
-
+    print(ws.title)
     title = ws.title.replace(' ', '')
-    # transaction schemas should be named after their id
-    if (isinstance(ws.cell(7, 2).value, str)
-            and ws.cell(7, 2).value.strip() == 'TRANSACTION TYPE IDENTIFIER'
-            and isinstance(ws.cell(7, 5).value, str)):
-        output_file = title + '-' + ws.cell(7, 5).value + '.json'
-    else:
-        output_file = title + ".json"
+    output_file = title + ".json"
 
     print(f'Parsing {output_file}...')
 
     schema = {
         "$schema": "http://json-schema.org/draft-07/schema#",
         "$id": f'{SCHEMA_ID_PREFIX}/{output_file}',
+        "version": VERSION,
         "title": f'FEC {ws.title}',
         "description": ws.cell(1, 1).value,
         "type": "object",
+        "required": [],
         "properties": {},
-        "additionalProperties": False,
-        "required": []
+        "additionalProperties": False
     }
 
-    for row in ws.iter_rows(min_row=5, max_col=7, values_only=True):
-        if (not row[COL_SEQ]
-                or row[COL_SEQ] == "--"
-                or not row[FIELD_DESCRIPTION]
-                or not row[TYPE]
+    sheet_has_autopopulate = ws.cell(3, 5).value is not None and ws.cell(3, 5).value.strip() == 'Auto populate'
+
+    for row in ws.iter_rows(min_row=5, max_col=8, values_only=True):
+        if (not Columns.COL_SEQ.get(row, sheet_has_autopopulate)
+                or Columns.COL_SEQ.get(row, sheet_has_autopopulate) == "--"
+                or not Columns.FIELD_DESCRIPTION.get(row, sheet_has_autopopulate)
+                or not Columns.TYPE.get(row, sheet_has_autopopulate)
                 or len(row) > 10):
             continue
-
         print(row)
-        token, prop = convert_row_to_property(row)
+        token, prop = convert_row_to_property(row, sheet_has_autopopulate)
+        if token == "TRANSACTION_TYPE_IDENTIFIER":
+            print(prop)
+            trans_type_id = prop.get('spec',{}).get(Columns.SAMPLE_DATA.name, "")
+            trans_type_hits[trans_type_id] = (trans_type_hits.get(trans_type_id) or 0) + 1
+            if (trans_type_hits[trans_type_id] > 1 or trans_type_id == ''):
+                output_file = trans_type_id + '-' + str(trans_type_hits[trans_type_id]) + '.json'
+            else:
+                output_file = trans_type_id + '.json'
         schema["properties"][token] = prop
 
     f = open(output_file, "w")
