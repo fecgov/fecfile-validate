0a349685a9f359c420444af9624ba18f8425ba80
Matt Travers <matt.travers@tcg.com>
2022-01-25 18:44:35 -0500

    Clean up and rearrange files

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-API_URL = os.environ.get('FEC_VALIDATE_URL', '127.0.0.1')
-API_PORT = int(os.environ.get('FEC_VALIDATE_PORT', 8001))
-
-LANGUAGE_CODE = 'en-us'
-
-# TIME_ZONE = 'UTC'
-TIME_ZONE = "America/New_York"
