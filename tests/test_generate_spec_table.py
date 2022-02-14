5ca2c2c6d006910536d437fd5a5008b849a3ad29
toddlees <todd.lees@tcg.com>
2022-02-14 16:19:02 -0500

    More coverage for util scripts

--- tests/test_generate_spec_table.py
+++ tests/test_generate_spec_table.py
@@ -1,7 +1,13 @@
 import subprocess
+import pytest
+import json
+import os
+
+
+
 
 def test_generates_table():
     proc = subprocess.run(['python3', 'bin/generate-spec-table.py', 'schema/F3X.json'], capture_output=True)
-    out = proc.stdout.decode("utf-8")
-    assert out.startswith("<!DOCTYPE html>")
-    assert out.endswith("<</table></body></html>>")
+    output = proc.stdout.decode("utf-8")
+    assert output.startswith("<!DOCTYPE html>")
+    assert proc.stderr.decode("utf-8") == ""
