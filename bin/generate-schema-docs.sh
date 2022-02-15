05636d52885a4a8f517bbab43a84975c8997a5b7
toddlees <97105825+toddlees@users.noreply.github.com>
2022-02-15 10:09:43 -0500

    Removed typo from generate docs script

--- bin/generate-schema-docs.sh
+++ bin/generate-schema-docs.sh
@@ -3,6 +3,6 @@
 # Utility script to create the data dictionary *.html files
 #
 # NOTE: Be sure to run from the fecfile-Validate/schema directory
-#E
+#
 SCHEMAS=$(ls -m *.json | tr -d ' ')
 generate-schema-doc ${SCHEMAS} ../docs
