28030bcdfafc5c255cfb0d705860f2196686a4b2
toddlees <todd.lees@tcg.com>
2022-02-15 09:45:43 -0500

    fixed tests to comply with flake8

--- tests/test_generate_starter_schema.py
+++ tests/test_generate_starter_schema.py
@@ -1,5 +1,11 @@
 import subprocess
 
+
 def test_generate_starter_schema():
-    proc = subprocess.run(['python3', 'bin/generate-starter-schema.py', 'schema/backlog/FEC_Format_v8.3.0.1.xlsx', '--sheets-to-generate=bin/sheets-to-generate.json'], capture_output=True)
+    proc = subprocess.run(
+        ['python3',
+         'bin/generate-starter-schema.py',
+         'schema/backlog/FEC_Format_v8.3.0.1.xlsx',
+         '--sheets-to-generate=bin/sheets-to-generate.json'
+         ], capture_output=True)
     assert proc.stderr.decode("utf-8") == ""
