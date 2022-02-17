4bd29b546ee2d65919be62dbe419db743805e46f
toddlees <todd.lees@tcg.com>
2022-02-17 10:14:12 -0500

    Adds code coverage for generate schema script

--- tests/test_generate_starter_schema.py
+++ tests/test_generate_starter_schema.py
@@ -2,10 +2,16 @@ import subprocess
 
 
 def test_generate_starter_schema():
-    proc = subprocess.run(
+    fec_format_proc = subprocess.run(
         ['python3',
          'bin/generate-starter-schema.py',
          'schema/backlog/FEC_Format_v8.3.0.1.xlsx',
          '--sheets-to-generate=bin/sheets-to-generate.json'
          ], capture_output=True)
-    assert proc.stderr.decode("utf-8") == ""
+    assert fec_format_proc.stderr.decode("utf-8") == ""
+    contacts_proc = subprocess.run(
+        ['python3',
+         'bin/generate-starter-schema.py',
+         'schema/backlog/Contacts_Specs\ -\ Revised.xlsx'
+         ], capture_output=True)
+    assert contacts_proc.stderr.decode("utf-8") == ""
