3b630a58d23739ea2aeec427feeb10ed17f6a390
toddlees <todd.lees@tcg.com>
2022-03-01 17:53:05 -0500

    formatting

--- fecfile_validate_python/src/fecfile_validate/validate.py
+++ fecfile_validate_python/src/fecfile_validate/validate.py
@@ -10,20 +10,23 @@ logger = logging.getLogger(__name__)
 
 def get_schema(schema_name):
     """Return form schema as JSON object
-    
+
     Args:
         schema_name (str): name of schema to retrieve file for
-        
+
     Returns:
         dict: JSON schema that matches the schema_name"""
-    schema_file = os.path.join(os.path.dirname(__file__), "schema/", f"{schema_name}.json")
+    schema_file = f"{schema_name}.json"
+    schema_path = os.path.join(
+        os.path.dirname(__file__), "schema/", schema_file
+    )
     #: Handle case where we are not running from a pip package
-    if not os.path.isfile(schema_file):
-        logger.warning(f"Schema file ({schema_file}) not found in package.")
-        schema_file = os.path.join(
-            os.path.dirname(__file__), "../../../schema/", f"{schema_name}.json"
+    if not os.path.isfile(schema_path):
+        logger.warning(f"Schema file ({schema_path}) not found in package.")
+        schema_path = os.path.join(
+            os.path.dirname(__file__), "../../../schema/", schema_file
         )
-    with open(schema_file) as fp:
+    with open(schema_path) as fp:
         form_schema = json.load(fp)
 
     return form_schema
@@ -33,8 +36,10 @@ def validate(schema_name, form_data):
     """Wrapper function around jsonschema validator
 
     Args:
-        schema_name (str): unique identifier that maps to the JSON schema definition file
-        data (dict): key-value pairs of data to be validated. Keys match the properties of the JSON schema file
+        schema_name (str): unique identifier that maps to the
+            JSON schema definition file
+        data (dict): key-value pairs of data to be validated.
+            Keys match the properties of the JSON schema file
 
     Returns:
         list of ValidationError: A list of all errors found in form_data"""
