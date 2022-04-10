8fc7f3fcaf8003a0776e38dc9ef24705c26eeefd
Matt Travers <matt.travers@tcg.com>
2022-04-10 08:53:53 -0400

    Cleaned up linting issues in unit test file

--- fecfile_validate_python/tests/test_contact_schema.py
+++ fecfile_validate_python/tests/test_contact_schema.py
@@ -1,9 +1,8 @@
 import pytest
 import json
 import os
-from jsonschema import Draft7Validator
 from src.fecfile_validate import validate
-import pdb
+
 
 @pytest.fixture
 def sample_ind_contact():
@@ -41,22 +40,21 @@ def test_required_candidate_state(sample_can_contact):
                                           sample_can_contact)
     assert validation_result.errors == []
 
-
     # Fail null for candidate state when office is S
     sample_can_contact["candidate_office"] = "S"
     validation_result = validate.validate("Contact_Candidate",
                                           sample_can_contact)
     assert validation_result.errors[0].path == "candidate_state"
-    assert validation_result.errors[0].message == "None is not of type 'string'"
-
+    assert validation_result.errors[0].message == \
+        "None is not of type 'string'"
 
     # Fail not having candidate state at all when office is S
     del sample_can_contact["candidate_state"]
     validation_result = validate.validate("Contact_Candidate",
                                           sample_can_contact)
     assert validation_result.errors[0].path == "candidate_state"
-    assert validation_result.errors[0].message == "'candidate_state' is a required property"
-
+    assert validation_result.errors[0].message == \
+        "'candidate_state' is a required property"
 
     # Fail null for candidate district when office is H
     sample_can_contact["candidate_state"] = "VA"
@@ -64,20 +62,21 @@ def test_required_candidate_state(sample_can_contact):
     validation_result = validate.validate("Contact_Candidate",
                                           sample_can_contact)
     assert validation_result.errors[0].path == "candidate_district"
-    assert validation_result.errors[0].message == "None is not of type 'string'"
-
+    assert validation_result.errors[0].message == \
+        "None is not of type 'string'"
 
     # Fail invalid candidate district code
     sample_can_contact["candidate_district"] = "ZZ"
     validation_result = validate.validate("Contact_Candidate",
                                           sample_can_contact)
     assert validation_result.errors[0].path == "candidate_district"
-    assert validation_result.errors[0].message == "'ZZ' does not match '^[0-9]{2}$'"
-
+    assert validation_result.errors[0].message == \
+        "'ZZ' does not match '^[0-9]{2}$'"
 
     # Fail not having a candidate district when office is H
     del sample_can_contact["candidate_district"]
     validation_result = validate.validate("Contact_Candidate",
                                           sample_can_contact)
     assert validation_result.errors[0].path == "candidate_district"
-    assert validation_result.errors[0].message == "'candidate_district' is a required property"
+    assert validation_result.errors[0].message == \
+        "'candidate_district' is a required property"
