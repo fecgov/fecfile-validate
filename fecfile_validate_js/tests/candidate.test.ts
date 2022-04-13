dc68ffd4c5569c814e2b5aad55b5a8215d4bebc1
Matt Travers <matt.travers@tcg.com>
2022-04-13 09:50:26 -0400

    Added unit testing to buildSchemaModules script

--- fecfile_validate_js/tests/candidate.test.ts
+++ fecfile_validate_js/tests/candidate.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.133.0/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 import { validate } from '../dist/index.js';
 import { schema as candidateContactSchema } from '../dist/Contact_Candidate.js';
 
