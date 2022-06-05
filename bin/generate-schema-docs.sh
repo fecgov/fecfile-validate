d77167d75a10377d39ce537fbcc17155397ddc26
Matt Travers <matt.travers@tcg.com>
2022-06-05 09:43:08 -0400

    Added validation rules to spec HTML files

--- bin/generate-schema-docs.sh
+++ bin/generate-schema-docs.sh
@@ -6,3 +6,14 @@
 #
 SCHEMAS=$(ls -m *.json | tr -d '[:space:]')
 generate-schema-doc ${SCHEMAS} ../docs
+
+# Remove fontawesome script that throws warnings in SonarCloud
+sed -e 's/<script src=https:\/\/use\.fontawesome\.com\/facf9fa52c\.js><\/script>//g' -i ../docs/*.html
+
+# Generate spec files
+for schema in ${SCHEMAS//,/ }
+do
+    # call your procedure/other scripts here below
+    target_file="${schema%.*}_spec.html"
+    python ../bin/generate-spec-table.py $schema > ../docs/$target_file
+done
--- bin/generate-spec-table.py
+++ bin/generate-spec-table.py
@@ -26,7 +26,8 @@ COLUMNS = [
     "SAMPLE DATA",
     "VALUE REFERENCE",
     "RULE REFERENCE",
-    "FIELD FORM ASSOCIATION"
+    "FIELD FORM ASSOCIATION",
+    "VALIDATION RULES"
 ]
 
 print(f'<!DOCTYPE html><html lang="en"><head><title>{title}</title>'
@@ -40,9 +41,46 @@ print('</tr>')
 for p in data['properties']:
     print('<tr>')
     for s in data['properties'][p]['fec_spec']:
+        if s == 'AUTO_POPULATE':
+            continue
         value = data["properties"][p]["fec_spec"][s]
         if not value:
             value = ''
         print(f'<td>{value}</td>')
+    validation_rules = '<ul>'
+
+    if 'allOf' in data:
+        for allOf in data['allOf']:
+            if p in allOf['then']['required']:
+                conditions = []
+                for q in allOf['if']['properties']:
+                    if 'const' in allOf['if']['properties'][q]:
+                        conditions.append(f"{q.upper()} equals {allOf['if']['properties'][q]['const']}")
+                    if 'enum' in allOf['if']['properties'][q]:
+                        conditions.append(f"{q.upper()} one of {allOf['if']['properties'][q]['enum']}")
+                    if 'minimum' in allOf['if']['properties'][q]:
+                        conditions.append(f"{q.upper()} >= {allOf['if']['properties'][q]['minimum']}")
+                validation_rules += f"<li>REQUIRED if {' & '.join(conditions)}</li>"
+
+    if p in data['required']:
+        validation_rules += "<li>REQUIRED</li>"
+    if 'type' in data['properties'][p]:
+        validation_rules += f"<li>type: {data['properties'][p]['type']}</li>"
+    if 'const' in data['properties'][p]:
+        validation_rules += f"<li>must equal: {data['properties'][p]['const']}</li>"
+    if 'enum' in data['properties'][p]:
+        validation_rules += f"<li>must be one of: {data['properties'][p]['enum']}</li>"
+    if 'minLength' in data['properties'][p]:
+        validation_rules += f"<li>min length: {data['properties'][p]['minLength']}</li>"
+    if 'maxLength' in data['properties'][p]:
+        validation_rules += f"<li>max length: {data['properties'][p]['maxLength']}</li>"
+    if 'pattern' in data['properties'][p]:
+        validation_rules += f"<li>regex: {data['properties'][p]['pattern']}</li>"
+    if 'minimum' in data['properties'][p]:
+        validation_rules += f"<li>minimum: {data['properties'][p]['minimum']}</li>"
+    if 'maximum' in data['properties'][p]:
+        validation_rules += f"<li>maximum: {data['properties'][p]['maximum']}</li>"
+
+    print(f'<td>{validation_rules}</td>')
     print('</tr>')
 print('</table></body></html>')
