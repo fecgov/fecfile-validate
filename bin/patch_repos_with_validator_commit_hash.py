2b5b2b2e225f4b78aebbdb08fa165b9930010732
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-25 11:20:48 -0400

    More updates to linting

--- bin/patch_repos_with_validator_commit_hash.py
+++ bin/patch_repos_with_validator_commit_hash.py
@@ -4,9 +4,9 @@ import time
 import pkg_resources
 from inspect import getsourcefile
 
-FRONT_END_ROOT_DIR= "fecfile-web-app"
+FRONT_END_ROOT_DIR = "fecfile-web-app"
 BACK_END_ROOT_DIR = "fecfile-web-api"
-VALIDATOR_ROOT_DIR= "fecfile-validate"
+VALIDATOR_ROOT_DIR = "fecfile-validate"
 FILE_PATH = os.path.abspath(getsourcefile(lambda: 0))
 # Get the directory three steps above the directory containing this file
 BASE_DIR = "/".join(FILE_PATH.split("/")[:-3])
@@ -20,9 +20,10 @@ def gitpython_is_installed():
             return True
     return False
 
+
 def get_commit_hash():
     import git
-    
+
     os.chdir(BASE_DIR)
     os.chdir(VALIDATOR_ROOT_DIR)
 
@@ -33,6 +34,7 @@ def get_commit_hash():
     sleep(1)
 
     return commit_hash
+
 
 def patch_app(commit_hash):
     os.chdir(BASE_DIR)
@@ -54,12 +56,13 @@ def patch_app(commit_hash):
             package_lines += line
 
     packages.close()
-    new_packages = open("package.json","w")
+    new_packages = open("package.json", "w")
     new_packages.write(package_lines)
     new_packages.close()
 
     print("Done!\n")
     sleep(0.5)
+
 
 def patch_api(commit_hash):
     os.chdir(BASE_DIR)
@@ -87,6 +90,7 @@ def patch_api(commit_hash):
     print("Done!\n")
     sleep(0.5)
 
+
 def delete_app_cache():
     print("Deleting .angular & node_modules...")
 
@@ -99,6 +103,7 @@ def delete_app_cache():
     print("Done!\nBe sure to run `npm install`\n")
     sleep(0.5)
 
+
 def delete_api_docker_images(docker_images):
     print("Deleting .angular & node_modules...")
     os.chdir(BASE_DIR)
@@ -106,15 +111,17 @@ def delete_api_docker_images(docker_images):
     print("Spinning down Docker...")
     os.system("docker-compose down")
     for image in docker_images:
-        print("Removing",image,"...")
+        print("Removing", image,"...")
         os.system("docker rmi "+image)
 
     print("Done!\nBe sure to run `docker-compose up`")
     sleep(0.5)
 
+
 def sleep(T):
     if "-q" not in sys.argv:
       time.sleep(T)
+
 
 def ask_true_false(question):
     if YES_TO_ALL:
@@ -133,24 +140,27 @@ def check_user_is_ready():
         print(padded_repo+BASE_DIR+"/"+repo)
     return ask_true_false("\nAre these directories correct?")
 
+
 def help():
     outString = """
+        This script uses the active branch of your fecfile-validate repo to update the validator commit hashes of your local app and api repos.  
+        Optionally, it also deletes the .angular and node_modules directories as well as the relevant docker images.
 
-This script uses the active branch of your fecfile-validate repo to update the validator commit hashes of your local app and api repos.  
-Optionally, it also deletes the .angular and node_modules directories as well as the relevant docker images.
+        For this script to work, it needs to be placed in the directory wherein each of the three repos can be found.
 
-For this script to work, it needs to be placed in the directory wherein each of the three repos can be found.
-
-Command line arguments:
-    -y or --yes |    auto-confirm all options"""
+        Command line arguments:
+            -y or --yes |    auto-confirm all options
+    """
     print(outString)
     return
-  
+
+
 def __main__():
     if "-h" in sys.argv or "--help" in sys.argv:
         help()
         return
     if "-y" in sys.argv or "--yes" in sys.argv:
+        global YS_TO_ALL
         YES_TO_ALL = True
 
     if not gitpython_is_installed():
@@ -160,18 +170,21 @@ def __main__():
 
     if not check_user_is_ready():
         return
-    
+
     commit_hash = get_commit_hash()
     patch_app(commit_hash)
     patch_api(commit_hash)
 
-    if ask_true_false("Would you like to delete the .angular and node_modules directories within "+FRONT_END_ROOT_DIR):
+    if ask_true_false(
+        "Would you like to delete the .angular and node_modules directories within "+
+        FRONT_END_ROOT_DIR
+    ):
         delete_app_cache()
 
     docker_images = ["fecfile-db","fecfile-api","fecfile-celery-worker", "redis:6.2-alpine"]
     if ask_true_false("Would you like to delete the following docker images?\n"+"\n".join(docker_images)+"\n"):
         delete_api_docker_images(docker_images)
-    
+
 
 if __name__ == "__main__":
     __main__()
