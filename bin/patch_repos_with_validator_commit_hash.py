0f5ebf4beb1732cb63221ab7f03d0956f8f0cbcb
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-25 11:30:06 -0400

    More updates to linting

--- bin/patch_repos_with_validator_commit_hash.py
+++ bin/patch_repos_with_validator_commit_hash.py
@@ -111,24 +111,24 @@ def delete_api_docker_images(docker_images):
     print("Spinning down Docker...")
     os.system("docker-compose down")
     for image in docker_images:
-        print("Removing", image,"...")
+        print("Removing", image, "...")
         os.system("docker rmi "+image)
 
     print("Done!\nBe sure to run `docker-compose up`")
     sleep(0.5)
 
 
-def sleep(T):
+def sleep(t):
     if "-q" not in sys.argv:
-      time.sleep(T)
+        time.sleep(t)
 
 
 def ask_true_false(question):
     if YES_TO_ALL:
         return True
 
-    response = input(question+" (y/N): ")  
-    if (len(response) == 0 or response.upper() not in ["Y","YES"]):
+    response = input(question+" (y/N): ")
+    if (len(response) == 0 or response.upper() not in ["Y", "YES"]):
         return False
     return True
 
@@ -142,25 +142,28 @@ def check_user_is_ready():
 
 
 def help():
-    outString = """
-        This script uses the active branch of your fecfile-validate repo to update the validator commit hashes of your local app and api repos.  
-        Optionally, it also deletes the .angular and node_modules directories as well as the relevant docker images.
+    help_string = """
+        This script uses the active branch of your fecfile-validate repo to
+        update the validator commit hashes of your local app and api repos.
+        Optionally, it also deletes the .angular and node_modules directories
+        as well as the relevant docker images.
 
-        For this script to work, it needs to be placed in the directory wherein each of the three repos can be found.
+        For this script to work, it needs to be placed in the directory
+        wherein each of the three repos can be found.
 
         Command line arguments:
             -y or --yes |    auto-confirm all options
     """
-    print(outString)
+    print(help_string)
     return
 
 
-def __main__():
+def main():
     if "-h" in sys.argv or "--help" in sys.argv:
         help()
         return
     if "-y" in sys.argv or "--yes" in sys.argv:
-        global YS_TO_ALL
+        global YES_TO_ALL
         YES_TO_ALL = True
 
     if not gitpython_is_installed():
@@ -176,15 +179,23 @@ def __main__():
     patch_api(commit_hash)
 
     if ask_true_false(
-        "Would you like to delete the .angular and node_modules directories within "+
+        "Would you like to delete the .angular and node_modules directories within " +
         FRONT_END_ROOT_DIR
     ):
         delete_app_cache()
 
-    docker_images = ["fecfile-db","fecfile-api","fecfile-celery-worker", "redis:6.2-alpine"]
-    if ask_true_false("Would you like to delete the following docker images?\n"+"\n".join(docker_images)+"\n"):
+    docker_images = [
+        "fecfile-db",
+        "fecfile-api",
+        "fecfile-celery-worker",
+        "redis:6.2-alpine"
+    ]
+    if ask_true_false(
+        "Would you like to delete the following docker images?\n" +
+        "\n".join(docker_images)+"\n"
+    ):
         delete_api_docker_images(docker_images)
 
 
 if __name__ == "__main__":
-    __main__()
+    main()
