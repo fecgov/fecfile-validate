58d86d9a812112025af8ee7918974ecb97266e71
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-10-03 16:20:34 -0400

    Linting updates...

--- fecfile_validate_python/tests/test_validate.py
+++ fecfile_validate_python/tests/test_validate.py
@@ -15,9 +15,9 @@ def sample_f3x():
 @pytest.fixture
 def sample_indv_rec():
     with open(
-            os.path.join(os.path.dirname(__file__),
-            "sample_individual_receipt.json")
-        ) as f:
+        os.path.join(os.path.dirname(__file__),
+        "sample_individual_receipt.json")
+    ) as f:
         form_data = json.load(f)
     return form_data
 
