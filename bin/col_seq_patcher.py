2aae67e74aeb22bc2cbdbc388674f3007368528c
Elaine Krauss <elaine.krauss@tcg.com>
2023-10-27 14:53:16 -0400

    Fixes a couple linting errors

--- bin/col_seq_patcher.py
+++ bin/col_seq_patcher.py
@@ -1,30 +1,38 @@
 import argparse
 
+# Column Sequence Patcher
+#
+#   Creates a copy of an existing JSON schema file
+#   where all COL_SEQ attributes have been updated
+#   to be sequential in order of appearance within
+#   the file.
+
 parser = argparse.ArgumentParser(
     prog='Column Sequence Patcher',
-    description='Creates a copy of a JSON schema file where\n'+
-        'all COL_SEQ attributes have been updated to\n'+
-        'be sequential in order of appearance within\n'+
+    description='Creates a copy of an existing JSON schema file\n' +
+        'where all COL_SEQ attributes have been updated\n' +
+        'to be sequential in order of appearance within\n' +
         'the file.'
 )
 
 parser.add_argument('filename')
 args = parser.parse_args()
 
-og_file = open(args.filename, 'r')
+old_file = open(args.filename, 'r')
 new_file = open("patched_"+args.filename, 'w')
 
-col_seq_count = 1
-for line in og_file:
+col_seq_count = 0
+for line in old_file:
     out_line = line
-    indentation = line.split('"')[0]
-    key = line.strip().split(":")[0]
-    if key and key == '"COL_SEQ"':
-        new_value = col_seq_count
-        col_seq_count += 1
-        out_line = indentation+key+": "+str(new_value)+",\n"
+    if "COL_SEQ" in line:
+        indentation = line.split('"')[0]
+        key = line.strip().split(":")[0]
+        if key and key == '"COL_SEQ"':
+            col_seq_count += 1
+            new_value = col_seq_count
+            out_line = indentation+key+": "+str(new_value)+",\n"
     new_file.write(out_line)
 
-print(f'Columns 1-{col_seq_count} found')
-og_file.close()
+print(f'COL_SEQ values 1 -> {col_seq_count} found and updated')
+old_file.close()
 new_file.close()
