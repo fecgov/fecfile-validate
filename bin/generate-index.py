5ef865d7edbbb6d9c76ef7c9f0313d1f21ab6eeb
Elaine Krauss <elaine.krauss@tcg.com>
2025-09-11 11:18:13 -0400

    Adds flags for testing and ignoring warnings; the test CircleCI job uses the --test flag; improves logging

--- bin/generate-index.py
+++ bin/generate-index.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 
@@ -8,6 +9,42 @@ EXCLUDED_FILES = [
     "_OVERRIDE_F99.json",
     "UNIT_TEST.json"
 ]
+
+
+def get_schema_map() -> dict:
+    if not os.path.isfile("../schema_map.json"):
+        raise FileNotFoundError("schema_map.json not found")
+
+    schema_map_file = open("../schema_map.json", "r")
+    schema_map = json.load(schema_map_file)
+    schema_map_file.close()
+
+    return schema_map
+
+
+# Returns a dict of file names with every value set to False
+# Used to track whether or not a file is referenced by the schema map
+def get_schema_files() -> dict:
+    schema_files = {}
+    for f in os.listdir():
+        if f[-5:] == ".json" and f not in EXCLUDED_FILES:
+            schema_files[f] = False
+
+    return schema_files
+
+
+def get_options() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=""
+        "Generates the validator documentation's index.html based on schema_map.json"
+    )
+    parser.add_argument(
+        "-t", "--test", help="Disables saving results to a file", action="store_true"
+    )
+    parser.add_argument(
+        "-w", "--ignore-warnings", help="Ignore warnings", action="store_true"
+    )
+    return parser.parse_args()
 
 
 def format_html(string, indentation_level=0):
@@ -31,7 +68,6 @@ def gen_html_for_link_specs(file_name, schema_files, invalid_links):
     else:
         spec_links["json"] = None
         invalid_links.append(file_name)
-        print(f"WARNING - Broken link in schema map - file doesn't exist: {file_name}.json")  # noqa: E501
 
     output_str = ""
     for spec_type in spec_links.keys():
@@ -106,66 +142,116 @@ def build_html_for_schema_category(schema_map, category, schema_files, invalid_l
     return output_str
 
 
-def gen_index_dot_html():
-    if not os.path.isfile("../schema_map.json"):
-        raise FileNotFoundError("schema_map.json not found")
+def gen_html(schema_map, schema_files, invalid_links):
+    html_for_head = build_html_for_head()
+    html_for_body = ""
+    for schema_category in schema_map.keys():
+        html_for_body += build_html_for_schema_category(
+            schema_map,
+            schema_category,
+            schema_files,
+            invalid_links
+        )
+    return [html_for_head, html_for_body]
 
-    schema_map_file = open("../schema_map.json", "r")
-    schema_map = json.load(schema_map_file)
-    schema_map_file.close()
 
-    schema_files = {}  # Values are whether or not files are found in schema_map
-    invalid_links = []
-
-    for f in os.listdir():
-        if f[-5:] == ".json" and f not in EXCLUDED_FILES:
-            schema_files[f] = False
-
-    print("Generating index.html ...")
+def write_to_file(html_body, html_head):
     output_file = open("../docs/index.html", "w")
 
     output_file.write(
         format_html('<!DOCTYPE html>\n<html lang=en>\n')
     )
 
-    output_file.write(build_html_for_head())
+    output_file.write(html_head)
 
     output_file.write(
         format_html('\n<body>\n<h2>FEC Form Data Dictionaries</h2>\n<hr>\n')
     )
 
-    for schema_category in schema_map.keys():
-        output_file.write(
-            build_html_for_schema_category(
-                schema_map,
-                schema_category,
-                schema_files,
-                invalid_links
-            )
-        )
+    output_file.write(html_body)
 
     output_file.write(
         format_html('</body>\n\n</html>')
     )
     output_file.close()
 
-    found_files = []
-    not_found_files = []
+
+def evaluate_run_status(schema_files, invalid_links):
+    warnings = []
+    found_files = 0
+    missing_files = 0
+
     for file_name in schema_files.keys():
-        if not schema_files.get(file_name):
-            print(f"WARNING - Json schema file not included in schema map: {file_name}")
-            not_found_files.append(file_name)
+        if not schema_files[file_name]:
+            warnings.append(
+                f"WARNING - Json schema file not included in schema map: {file_name}"
+            )
+            missing_files += 1
         else:
-            found_files.append(file_name)
+            found_files += 1
 
-    print(f"  - Schema files found in schema map: {len(found_files)}")
-    print(f"  - Schema files not found in schema map: {len(not_found_files)}")
-    print(f"  - Invalid links in Schema Map: {len(invalid_links)}")
+    for file_name in invalid_links:
+        warnings.append(
+            f"WARNING - Broken link in schema map - file doesn't exist: {file_name}.json"
+        )
 
-    if len(not_found_files) + len(invalid_links) > 0:
-        raise RuntimeError("Failed to generate index.html")
+    return {
+        "warnings": warnings,
+        "found_files": found_files,
+        "missing_files": missing_files
+    }
+
+
+def print_status(message, value, width):
+    print(
+        "  -",
+        message.ljust(width),
+        str(value).rjust(3)
+    )
+
+def print_status_messages(schema_files, status, invalid_links, width):
+    print_status("Files present in schema directory:", len(schema_files.keys()) + len(EXCLUDED_FILES), width)  # noqa: E501
+    print_status("Files excluded from search:", len(EXCLUDED_FILES), width)
+    print_status("Files found in schema map:", status['found_files'], width)
+    print_status("Files missing from schema map:", status['missing_files'], width)
+    print_status("Invalid links in Schema Map:", len(invalid_links), width)
+    print()  # Empty line for readability
+
+
+def gen_index_dot_html():
+    options = get_options()
+    schema_map = get_schema_map()
+    schema_files = get_schema_files()
+    invalid_links = []
+
+    print("Generating index.html ...\n")
+    print()
+
+    html_for_head, html_for_body = gen_html(schema_map, schema_files, invalid_links)
+
+    status = evaluate_run_status(schema_files, invalid_links)
+    
+    for warning in status["warnings"]:
+        print(warning)
+    if len(status["warnings"]) > 0:
+        print()
+
+    print_status_messages(schema_files, status, invalid_links, 36)
+
+    if len(status["warnings"]) > 0:
+        if not options.ignore_warnings:
+            raise RuntimeError(f"Failed to generate index.html: {len(status['warnings'])} warnings")  # noqa: E501
+        else:
+            print(f"Ignored {len(status['warnings'])} warnings ...")
+
+    if not options.test:
+        print("Saving index.html ...")
+        write_to_file(html_for_head, html_for_body)
+        print("Saved successfully\n")
     else:
-        print("Successfully generated index.html")
+        print("Skipped saving index.html\n")
+
+    print("Successfully generated index.html")
 
 
 if __name__ == "__main__":
